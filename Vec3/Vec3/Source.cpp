3802d730f4ab9030dadd39f428ac3ecdf4daf148
polferrando98 <pol.ferrando.98@gmail.com>
2017-09-19 14:06:50 +0200

    Worked on vec3

--- Vec3/Vec3/Source.cpp
+++ Vec3/Vec3/Source.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "Vec3.h"
 using namespace std;
 
 int main() {
+	Vec3<int> v1;
+	Vec3<int> v2;
 
+	Vec3<int> result;
+
+	v1.setCoords(1, 1, 1);
+
+	
+	
+	getchar();
 	return 0;
 }
--- Vec3/Vec3/Vec3.h
+++ Vec3/Vec3/Vec3.h
@@ -1,9 +1,47 @@
 #pragma once
-class Vec3
-{
+template <class T>
+class Vec3 {
 public:
+	void print();
+	inline void setCoords(T x, T y, T z);
+	Vec3 operator + (const Vec3 arg);
+	//Vec3 operator - (const Vec3 arg);
+	//Vec3& operator += (const Vec3 arg);
+	//Vec3& operator -= (const Vec3 arg);
+	//Vec3& operator = (const Vec3 arg);
+	//bool operator == (const Vec3 arg);
 
-	Vec3();
-	~Vec3();
+private:
+	T x, y, z;
+
 };
+
+#include "Vec3.h"
+#include <stdio.h>
+
+
+template<class T>
+void Vec3<T>::print()
+{
+	printf("(%d,%d,%d)", x, y, z);
+}
+
+template<class T>
+inline void Vec3<T>::setCoords(T x, T y, T z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+template<class T>
+Vec3<T> Vec3<T>::operator+(const Vec3 arg)
+{
+	Vec3 result = {
+		x + arg.x,
+		y + arg.y,
+		z + arg.z,
+	}
+}
+
 
